82c126d0ae00e37b400ae80edeef8ddac8f911f3
giannisgkountras <gkountrasioannis@gmail.com>
2022-12-13 23:10:24 +0200

    VroomVolts READY (not backend functionality)

--- src/App.js
+++ src/App.js
@@ -3,28 +3,33 @@ import Menu from "./components/Menu/Menu";
 import Dashboard from "./components/Dashboard/Dashboard";
 import ActivityPanel from "./components/ActivityPanel/ActivityPanel";
 import VroomVolts from "./components/VroomVolts/VroomVolts";
-import { BrowserRouter as Router, Switch, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Routes,
+} from "react-router-dom";
 
 function App() {
-    return (
-        <Router>
-            <div className="App">
-                <div className="menu-container">
-                    <Menu />
-                </div>
-                <div className="dashboard-container">
-                    {/* <Dashboard /> */}
-                    <Routes>
-                        <Route path="/" element={<Dashboard />} />
-                        <Route path="/volts" element={<VroomVolts />} />
-                    </Routes>
-                </div>
-                <div className="activity-container">
-                    <ActivityPanel />
-                </div>
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <div className="App">
+        <div className="menu-container">
+          <Menu />
+        </div>
+        <div className="dashboard-container">
+          {/* <Dashboard /> */}
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/volts" element={<VroomVolts />} />
+          </Routes>
+        </div>
+        <div className="activity-container">
+          <ActivityPanel />
+        </div>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
