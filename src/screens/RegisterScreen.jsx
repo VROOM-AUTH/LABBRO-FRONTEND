1ab07ec5f536502d46b393dda32c8c14e198d8ff
giannishorgos <giannishorgos@gmail.com>
2023-01-14 17:19:10 +0200

    minor fixes

--- src/screens/RegisterScreen.jsx
+++ src/screens/RegisterScreen.jsx
@@ -1,16 +1,16 @@
-import "./LoginReg.css";
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router";
-import LBL from "../assets/LabBro_Logo.png";
-import VL from "../assets/Vroom Logo.png";
-var CryptoJS = require("crypto-js");
+import './LoginReg.css';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router';
+import LBL from '../assets/LabBro_Logo.png';
+import VL from '../assets/Vroom Logo.png';
+var CryptoJS = require('crypto-js');
 
 const RegisterScreen = ({}) => {
-    const [pwd, setPwd] = useState("");
-    const [name, setName] = useState("");
-    const [hashedPwd, setHashedPwd] = useState("");
+    const [pwd, setPwd] = useState('');
+    const [name, setName] = useState('');
+    const [hashedPwd, setHashedPwd] = useState('');
     const Navigate = useNavigate();
-    const bcrypt = require("bcryptjs");
+    const bcrypt = require('bcryptjs');
 
     // const hashCode = (s) =>
     //     s.split("").reduce((a, b) => ((a << 5) - a + b.charCodeAt(0)) | 0, 0);
@@ -28,19 +28,19 @@ const RegisterScreen = ({}) => {
                 })
                 .then((data) => {
                     // console.log(data);
-                    if (data[0].password == "" && data[0].name != null) {
+                    if (!data[0].password && data[0].name !== null) {
                         fetch(
                             `${process.env.REACT_APP_BASE_URL}users/${data[0].id}`,
                             {
-                                method: "PUT",
-                                headers: { "Content-Type": "application/json" },
+                                method: 'PUT',
+                                headers: { 'Content-Type': 'application/json' },
                                 body: JSON.stringify({
                                     password: hashedPwd,
                                 }),
                             }
                         );
                         // console.log("Register");
-                        Navigate("/login");
+                        Navigate('/login');
                     }
                 })
                 .catch((error) => {
@@ -67,57 +67,53 @@ const RegisterScreen = ({}) => {
     //     Navigate("/login");
     // };
     return (
-        <div className="big-container">
-            <div className="column box">
-                <h1 className="loginreg-title h1">
+        <div className='big-container'>
+            <div className='column box'>
+                <h1 className='loginreg-title h1'>
                     Register to Lab Bro Portal
                 </h1>
-                <div className="container">
-                    <form onSubmit={handleSignup} className="column">
+                <div className='container'>
+                    <form onSubmit={handleSignup} className='column'>
                         <input
-                            className="labels input h2"
-                            placeholder="  Name"
-                            type="text"
-                            id="name"
+                            className='labels input h2'
+                            placeholder='  Name'
+                            type='text'
+                            id='name'
                             onChange={(event) => setName(event.target.value)}
                             value={name}
                         />
 
                         <input
-                            className="labels input h2"
-                            placeholder="  Password"
-                            type="password"
-                            id="password"
+                            className='labels input h2'
+                            placeholder='  Password'
+                            type='password'
+                            id='password'
                             onChange={(event) => setPwd(event.target.value)}
                             value={pwd}
                             required
                         />
                         <button
-                            className="mainButton h3"
-                            onSubmit={handleSignup}
-                        >
+                            className='mainButton h3'
+                            onSubmit={handleSignup}>
                             Register
                         </button>
                         <button
-                            className="secondButton h3"
-                            onClick={() => Navigate("/login")}
-                        >
+                            className='secondButton h3'
+                            onClick={() => Navigate('/login')}>
                             Login
                         </button>
                     </form>
-                    <div className="columnB">
+                    <div className='columnB'>
                         <img
-                            className="bro"
-                            alt="Bro"
-                            height="200px"
-                            src={LBL}
-                        ></img>
+                            className='bro'
+                            alt='Bro'
+                            height='200px'
+                            src={LBL}></img>
                         <img
-                            className="vroom"
-                            alt="vroom"
-                            height="250px"
-                            src={VL}
-                        ></img>
+                            className='vroom'
+                            alt='vroom'
+                            height='250px'
+                            src={VL}></img>
                     </div>
                 </div>
             </div>
