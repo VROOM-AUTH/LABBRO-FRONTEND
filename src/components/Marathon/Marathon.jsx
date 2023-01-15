e2838295fe06dc12c70a82561e883da6ba5240bb
giannishorgos <giannishorgos@gmail.com>
2023-01-15 15:41:26 +0200

    styling fixes

--- src/components/Marathon/Marathon.jsx
+++ src/components/Marathon/Marathon.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from "react";
-import "./Marathon.css";
-import Confetti from "../Confetti/Confetti";
-import first from "../../assets/first.png";
-import second from "../../assets/second.png";
-import third from "../../assets/third.png";
+import React, { useEffect, useState } from 'react';
+import './Marathon.css';
+import Confetti from '../Confetti/Confetti';
+import first from '../../assets/first.png';
+import second from '../../assets/second.png';
+import third from '../../assets/third.png';
 export default function Marathon({ userData }) {
     const [loading1, setLoading1] = useState(true);
     const [loading2, setLoading2] = useState(true);
@@ -45,23 +45,23 @@ export default function Marathon({ userData }) {
         const minutes = Math.floor((seconds % 3600) / 60);
         const remainingSeconds = seconds % 60;
 
-        return hours + " hours and " + minutes + " minutes ";
+        return hours + ' hours and ' + minutes + ' minutes ';
     }
 
     return (
-        <div className="marathon">
+        <div className='marathon'>
             {loading1 || loading2 ? (
                 <Confetti />
             ) : (
                 <>
-                    <div className="marathon-header">
+                    <div className='marathon-header'>
                         <h1>Marathon</h1>
                     </div>
-                    <div className="leaderboard-container">
+                    <div className='leaderboard-container'>
                         <Confetti />
-                        <div className="cards-container">
-                            <div className="marathon-card">
-                                <h1 className="second">
+                        <div className='cards-container'>
+                            <div className='marathon-card'>
+                                <h1 className='second'>
                                     {
                                         idToName.find(
                                             (o) =>
@@ -71,17 +71,16 @@ export default function Marathon({ userData }) {
                                 </h1>
                                 <img
                                     src={second}
-                                    alt="first"
-                                    width="150vw"
-                                ></img>
+                                    alt='first'
+                                    width='150vw'></img>
                                 <p>
                                     {convertSeconds(
                                         marathonData[1].total_seconds
                                     )}
                                 </p>
                             </div>
-                            <div className="marathon-card">
-                                <h1 className="first">
+                            <div className='marathon-card'>
+                                <h1 className='first'>
                                     {
                                         idToName.find(
                                             (o) =>
@@ -91,17 +90,16 @@ export default function Marathon({ userData }) {
                                 </h1>
                                 <img
                                     src={first}
-                                    alt="first"
-                                    width="300vw"
-                                ></img>
+                                    alt='first'
+                                    width='300vw'></img>
                                 <p>
                                     {convertSeconds(
                                         marathonData[0].total_seconds
                                     )}
                                 </p>
                             </div>
-                            <div className="marathon-card">
-                                <h1 className="third">
+                            <div className='marathon-card'>
+                                <h1 className='third'>
                                     {
                                         idToName.find(
                                             (o) =>
@@ -111,9 +109,8 @@ export default function Marathon({ userData }) {
                                 </h1>
                                 <img
                                     src={third}
-                                    alt="first"
-                                    width="100px"
-                                ></img>
+                                    alt='first'
+                                    width='100px'></img>
                                 <p>
                                     {convertSeconds(
                                         marathonData[2].total_seconds
@@ -124,15 +121,14 @@ export default function Marathon({ userData }) {
                     </div>
                     <div></div>
                     {userData.isLoggedIn ? (
-                        <div className="center">
-                            <h1 className="position">
+                        <div className='center'>
+                            <h1 className='position'>
                                 {/* TRANSFORMS the array to an array of ids and finds the index of the logged in user id. Plus one because index starts at 0 */}
-                                You are {""}
+                                You are {''}
                                 <span
                                     style={{
-                                        color: "rgba(194, 89, 228, 1)",
-                                    }}
-                                >
+                                        color: 'rgba(194, 89, 228, 1)',
+                                    }}>
                                     {marathonData
                                         .map((e) => e.user_id)
                                         .indexOf(userData.userId) + 1}
@@ -142,44 +138,44 @@ export default function Marathon({ userData }) {
                                         .indexOf(userData.userId) +
                                         1 ===
                                     1
-                                        ? "st "
+                                        ? 'st '
                                         : marathonData
                                               .map((e) => e.user_id)
                                               .indexOf(userData.userId) +
                                               1 ===
                                           2
-                                        ? "nd "
+                                        ? 'nd '
                                         : marathonData
                                               .map((e) => e.user_id)
                                               .indexOf(userData.userId) +
                                               1 ===
                                           3
-                                        ? "rd "
-                                        : "th "}{" "}
+                                        ? 'rd '
+                                        : 'th '}{' '}
                                 </span>
                                 on the leaderboard.
                             </h1>
-                            <h1 className="total-time">
-                                You have spent a total time of{" "}
-                                <span
+                            <h1 className='total-time'>
+                                You have spent a total time of{' '}
+                                {/* <span
                                     style={{
-                                        color: "rgba(194, 89, 228, 1)",
-                                    }}
-                                >
+                                        color: 'rgba(194, 89, 228, 1)',
+                                    }}>
                                     {convertSeconds(
                                         marathonData[
                                             marathonData
                                                 .map((e) => e.user_id)
                                                 .indexOf(userData.userId)
                                         ].total_seconds
-                                    )}{" "}
-                                </span>
+                                    )}
+                                    {}
+                                </span> */}
                                 in the lab!
                             </h1>
                         </div>
                     ) : (
-                        <div className="center">
-                            <h1 className="login-for-position">
+                        <div className='center'>
+                            <h1 className='login-for-position'>
                                 Login to view your position on the leaderboard
                                 and your total time spent in the lab!
                             </h1>
