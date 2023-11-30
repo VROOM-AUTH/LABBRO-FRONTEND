3ca353603e9a8c80461a991c4cfdfc3c08fb5719
giannisgkountras <gkountrasioannis@gmail.com>
2023-11-30 21:53:56 +0200

    Login is checked from the database

--- src/Screens/LoginScreen/LoginScreen.jsx
+++ src/Screens/LoginScreen/LoginScreen.jsx
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
-import "./LoginScreen.css";
+import bcrypt from "bcryptjs-react";
+
 import { FaUser, FaLock } from "react-icons/fa";
-import labbroLogo from "../../Assets/labbro-logo.png";
+import "./LoginScreen.css";
+
 export default function LoginScreen({ setIsLogin }) {
     const [rememberMe, setRememberMe] = useState(false);
+    const [name, setName] = useState("");
+    const [pwd, setPwd] = useState("");
 
     const handleRememberMeChange = () => {
         setRememberMe(!rememberMe);
@@ -11,12 +15,45 @@ export default function LoginScreen({ setIsLogin }) {
 
     const handleLogin = (event) => {
         event.preventDefault();
-        setTimeout(() => {
-            setIsLogin(true);
-            if (rememberMe) {
-                localStorage.setItem("isLogin", true);
-            }
-        }, 200);
+        try {
+            fetch(`${process.env.REACT_APP_BASE_URL}users/?name=${name}`, {
+                method: "GET",
+                headers: {
+                    // "Access-Control-Allow-Origin": "*",
+                    "Content-Type": "application/json",
+                    Authorization: `Token ${process.env.REACT_APP_AUTH_TOKEN}`,
+                },
+            })
+                .then((response) => {
+                    if (response.ok) {
+                        return response.json();
+                    }
+                    throw response;
+                })
+                .then((data) => {
+                    bcrypt.compare(pwd, data[0].password, function (err, res) {
+                        if (res) {
+                            console.log("Password matches.");
+                            setIsLogin(true);
+                            if (rememberMe) {
+                                localStorage.setItem("isLogin", true);
+                            }
+                        } else {
+                            alert("Incorrect password!");
+
+                            console.log("Password does not match.");
+                        }
+                    });
+                })
+                .catch((error) => {
+                    console.log(error);
+                    alert("Username does not exist!");
+                });
+        } catch (error) {
+            alert(error.message);
+        }
+
+        setTimeout(() => {}, 200);
     };
 
     return (
@@ -26,9 +63,25 @@ export default function LoginScreen({ setIsLogin }) {
                 <h1 className="login-form-title">LOGIN</h1>
                 <div className="login-form-group">
                     <FaUser className="login-form-icon1" />
-                    <input type="name" id="name" placeholder="      Username" />
+                    <input
+                        type="name"
+                        id="name"
+                        placeholder="      Username"
+                        onChange={(event) => {
+                            setName(event.target.value);
+                        }}
+                        required
+                    />
                     <FaLock className="login-form-icon2" />
-                    <input type="password" id="password" placeholder="      Password" />
+                    <input
+                        type="password"
+                        id="password"
+                        placeholder="      Password"
+                        onChange={(event) => {
+                            setPwd(event.target.value);
+                        }}
+                        required
+                    />
                 </div>
                 <div>
                     <button className="login-form-btn" onClick={(e) => handleLogin(e)}>
