98b9fb8cbe6b34feb680d168661d75772cd68812
giannisgkountras <gkountrasioannis@gmail.com>
2023-12-05 17:51:46 +0200

    Added dashboard cards

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import "./Dashboard.css";
 import secondsToHoursMins from "../../Utils/secondsToHoursMins";
 import formatDate from "../../Utils/formatDate";
-import { FaLock, FaLockOpen } from "react-icons/fa";
+import { FaLock, FaLockOpen, FaClock, FaRegCalendarAlt } from "react-icons/fa";
+import { IoPerson } from "react-icons/io5";
+import { FaDoorClosed } from "react-icons/fa6";
 
 export default function Dashboard({ labStatus, mergedUsers }) {
-    console.log(mergedUsers);
+    const labCount = mergedUsers.filter((item) => item.in_lab).length;
+    const totalUsers = mergedUsers.length;
 
     return (
         <div className="dashboard-container">
@@ -18,10 +21,31 @@ export default function Dashboard({ labStatus, mergedUsers }) {
                     </h1>
                 </div>
 
-                <p>Opened time is {formatDate(labStatus.opened_time)}</p>
-                <p>{labStatus.closed_time === "Lab Still Open" ? "Lab is Open" : `Lab closed ${formatDate(labStatus.closed_time)}`}</p>
-                <p>Total lab time is {secondsToHoursMins(labStatus.total_seconds)}</p>
-                <p></p>
+                <div className="dashboard-cards-container">
+                    <div className="dashboard-card">
+                        <FaClock className="card-icon" />
+                        <p className="card-text">{secondsToHoursMins(labStatus.total_seconds)}</p>
+                        <p className="card-text-secondary">Total open time</p>
+                    </div>
+
+                    <div className="dashboard-card">
+                        <IoPerson className="card-icon" />
+                        <p className="card-text">
+                            {labCount} out of {totalUsers}
+                        </p>
+                        <p className="card-text-secondary">Users in lab</p>
+                    </div>
+                    <div className="dashboard-card">
+                        <FaRegCalendarAlt className="card-icon" />
+                        <p className="card-text">{formatDate(labStatus.opened_time)}</p>
+                        <p className="card-text-secondary">Lab last opened</p>
+                    </div>
+                    <div className="dashboard-card">
+                        <FaDoorClosed className="card-icon" />
+                        <p className="card-text">{labStatus.closed_time === "Lab Still Open" ? "Still open!" : `Lab closed ${formatDate(labStatus.closed_time)}`}</p>
+                        <p className="card-text-secondary">Lab last closed</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
