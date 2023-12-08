b7c38aa79660f6bc76e2104bd5b95d6e10a0528d
giannisgkountras <gkountrasioannis@gmail.com>
2023-12-08 14:03:42 +0200

    Show only users in lab button added

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -1,24 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Users.css";
 
 export default function Users({ mergedUsers }) {
+    const usersInLab = mergedUsers.filter((user) => user.in_lab === true);
+    const [showOnlyInLab, setShowOnlyInLab] = useState(false);
     const User = ({ user }) => {
         return (
             <div className="user-container">
-                <strong>{user.name}</strong> - Vroomvolts: {user.vroomvolts} In
-                Lab:{" "}
-                {user.in_lab ? (
-                    <span style={{ color: "green" }}>Yes</span>
-                ) : (
-                    <span>No</span>
-                )}
+                <strong>{user.name}</strong> - Vroomvolts: {user.vroomvolts} In Lab: {user.in_lab ? <span style={{ color: "green" }}>Yes</span> : <span>No</span>}
             </div>
         );
     };
     return (
         <div>
-            {mergedUsers.map((user) => (
-                <User key={user.id} user={user} />
-            ))}
+            <button onClick={() => setShowOnlyInLab(!showOnlyInLab)}>Show only users in lab</button>
+
+            {showOnlyInLab ? (
+                <div>
+                    <h1>Users in lab:</h1>
+                    {usersInLab.map((user) => (
+                        <User key={user.id} user={user} />
+                    ))}
+                </div>
+            ) : (
+                <div>
+                    <h1>All Users:</h1>
+                    {mergedUsers.map((user) => (
+                        <User key={user.id} user={user} />
+                    ))}
+                </div>
+            )}
         </div>
     );
 }
