d56c23cf7190b7418a37927289ce368fbac7f8c5
giannisgkountras <gkountrasioannis@gmail.com>
2023-12-14 22:16:41 +0200

    Added basic logic in blackjack game

--- src/Components/Games/Blackjack.jsx
+++ src/Components/Games/Blackjack.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./Blackjack.css";
 import coin from "../../Assets/coin.png";
 import { IoArrowBackCircle } from "react-icons/io5";
@@ -8,18 +8,41 @@ import randomCard from "../../Utils/randomCard";
 import cardValue from "../../Utils/cardValue";
 export default function Blackjack({ userVroomVolts, setUserVroomVolts }) {
     const Navigate = useNavigate();
-    const [playerCards, setPlayerCards] = React.useState([]);
-    const [playerScore, setPlayerScore] = React.useState(0);
+    const [playerCards, setPlayerCards] = useState([]);
+    const [manaCards, setManaCards] = useState(["2B", "2B"]);
+    const [playerScore, setPlayerScore] = useState(0);
+    const [manaScore, setManaScore] = useState(0);
+    const [bet, setBet] = useState(20);
+    const [startGame, setStartGame] = useState(false);
 
     const addCard = () => {
         setPlayerCards([...playerCards, randomCard()]);
     };
+
+    const evaluateGame = (manaScore, playerScore) => {
+        if (manaScore > playerScore) {
+            alert("You lost!");
+            setStartGame(false);
+        } else {
+            alert("You won!");
+            setUserVroomVolts(userVroomVolts + bet * 2);
+            setStartGame(false);
+        }
+    };
+
+    useEffect(() => {
+        setPlayerCards([randomCard(), randomCard()]);
+    }, [startGame]);
 
     useEffect(() => {
         let score = 0;
         playerCards.forEach((card) => {
             score += cardValue(card);
         });
+        if (score > 21) {
+            alert("You lost!");
+            setStartGame(false);
+        }
         setPlayerScore(score);
     }, [playerCards]);
     return (
@@ -36,16 +59,61 @@ export default function Blackjack({ userVroomVolts, setUserVroomVolts }) {
                         <h1>{userVroomVolts}</h1> <img src={coin} alt='coin'></img>
                     </div>
                 </div>
+                <div className='blackjack-instructions'>If you win you gain x2 your bet!</div>
                 <div className='blackjack-mana-container'>
-                    <PlayingCard card={"AH"} />
-                </div>
-                <div className='blackjack-player-container'>
-                    {playerCards.map((card) => (
+                    {manaCards.map((card) => (
                         <PlayingCard card={card} />
                     ))}
                 </div>
-                <div onClick={addCard}>ADD MORE</div>
-                <div className='blackjack-instructions'>Score : {playerScore}</div>
+                <div className='blackjack-player-container'>
+                    {startGame ? (
+                        <>
+                            <div className='cards-row'>
+                                {playerCards.map((card) => (
+                                    <PlayingCard card={card} />
+                                ))}
+                            </div>
+                            <div className='blackjack-game-buttons'>
+                                <div className='blackjack-bet-button' onClick={addCard}>
+                                    Hit
+                                </div>
+                                <div className='blackjack-bet-button' onClick={() => evaluateGame(manaScore, playerScore)}>
+                                    Done
+                                </div>
+                            </div>
+
+                            <div className='blackjack-score'>SCORE : {playerScore}</div>
+                        </>
+                    ) : (
+                        <div className='blackjack-bet-container'>
+                            <h1>Place your bet</h1>
+                            <input
+                                defaultValue={bet}
+                                className='bet-input'
+                                type='number'
+                                placeholder='Bet'
+                                step='5'
+                                min='20'
+                                max='1000'
+                                onChange={(e) => {
+                                    setBet(e.target.value);
+                                }}
+                            ></input>
+
+                            <div
+                                className='blackjack-bet-button'
+                                onClick={() => {
+                                    if (bet >= 20 && bet <= userVroomVolts) {
+                                        setStartGame(true);
+                                        setUserVroomVolts(userVroomVolts - bet);
+                                    }
+                                }}
+                            >
+                                Start
+                            </div>
+                        </div>
+                    )}
+                </div>
             </div>
         </div>
     );
