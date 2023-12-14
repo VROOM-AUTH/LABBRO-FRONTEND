6964fb6d76b340c8d93a66e28ff2d1368076354e
giannisgkountras <gkountrasioannis@gmail.com>
2023-12-14 20:16:11 +0200

    Created component that returns playing card based on string

--- src/Components/Games/Blackjack.jsx
+++ src/Components/Games/Blackjack.jsx
@@ -3,6 +3,7 @@ import "./Blackjack.css";
 import coin from "../../Assets/coin.png";
 import { IoArrowBackCircle } from "react-icons/io5";
 import { useNavigate } from "react-router-dom";
+import PlayingCard from "../../Utils/PlayingCard";
 
 export default function Blackjack({ userVroomVolts, setUserVroomVolts }) {
     const Navigate = useNavigate();
@@ -21,6 +22,9 @@ export default function Blackjack({ userVroomVolts, setUserVroomVolts }) {
                         <h1>{userVroomVolts}</h1> <img src={coin} alt='coin'></img>
                     </div>
                 </div>
+                <PlayingCard card={"AH"} />
+                <PlayingCard card={"JD"} />
+                <PlayingCard card={"3S"} />
                 <div className='blackjack-instructions'>Each spin costs 5 Vroomvolts!</div>
             </div>
         </div>
